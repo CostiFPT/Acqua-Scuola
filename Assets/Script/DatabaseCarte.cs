564cd893613386369d44534344d2be71abd409b2
bgch69 <106741307+bgch69@users.noreply.github.com>
2024-03-15 09:50:27 +0100

    Update DatabaseCarte.cs
    aggiunte carte al database

--- Assets/Script/DatabaseCarte.cs
+++ Assets/Script/DatabaseCarte.cs
@@ -10,13 +10,18 @@ public class DatabaseCarte : MonoBehaviour
     {
         cardList.Add(new Carta(0, "Goccia d'Acqua", "Una goccia d'acqua pulita e potabile.", Resources.Load<Sprite>("acquaplaceholder")));
         cardList.Add(new Carta(1, "Goccia d'Acqua Sporca", "Una goccia d'acqua sporca da purificare.", Resources.Load<Sprite>("acquasporcaplaceholder")));
-        cardList.Add(new Carta(2, "Goccia d'Acqua Economica", "Offri due carte dalla tua mano al tuo avversario e in cambio prendi una carta goccia dal campo avversario.", Resources.Load<Sprite>("acquaplaceholder")));
-        cardList.Add(new Carta(3, "Accesso Universale", "Si.", Resources.Load<Sprite>("acquasporcaplaceholder")));
+        cardList.Add(new Carta(2, "Goccia d'Acqua Economica", "Offri una carta dalla tua mano al tuo avversario e in cambio prendi una carta goccia dal campo avversario e posizionala nel tuo campo.", Resources.Load<Sprite>("acquaplaceholder")));
+        cardList.Add(new Carta(3, "Accesso Universale", "Viene applicata l'abilità di una tua carta a scelta senza giocarla.", Resources.Load<Sprite>("acquasporcaplaceholder")));
         cardList.Add(new Carta(4, "WC", "Pesca casualmente una carta dalla mano del tuo avversario e mettila nel mazzo degli scarti.", Resources.Load<Sprite>("wcplaceholder")));
         cardList.Add(new Carta(5, "Inquinamento", "Rendi una carta dell'avversario da 'Goccia d'Acqua' a 'Goccia d'Acqua Sporca'.", Resources.Load<Sprite>("inquinamentoplaceholder")));
         cardList.Add(new Carta(6, "Riciclo e Riutilizzo", "Prendi una carta a tua scelta dal mazzo degli scarti e posizionala nella tua mano.", Resources.Load<Sprite>("ricicloplaceholder")));
-        cardList.Add(new Carta(7, "Depuratore", "Scarta due carte dalla tua mano e purifica le gocce d'acqua presenti sul tuo campo rendendole potabili.", Resources.Load<Sprite>("depuratoreplaceholder")));
+        cardList.Add(new Carta(7, "Depuratore", "Scarta due carte dalla tua mano e purifica le gocce d'acqua presenti sul tuo campo rendendole 'Gocce d'Acqua'.", Resources.Load<Sprite>("depuratoreplaceholder")));
         cardList.Add(new Carta(8, "Rilascio di Sostanze", "Posiziona una tua carta nella mano dell'avversario, obbligandolo ad usarla nel turno successivo.", Resources.Load<Sprite>("rilasciodisostanzeplaceholder")));
         cardList.Add(new Carta(9, "Materiali Pericolosi", "Posiziona due carte dalla tua mano al mazzo degli scarti.", Resources.Load<Sprite>("materialipericolosiplaceholder")));
+        cardList.Add(new Carta(10, "Acque Reflue", "Cedi una carta a scelta dal tuo campo al campo avversario.", Resources.Load<Sprite>("materialipericolosiplaceholder")));
+        cardList.Add(new Carta(11, "Carenza Idrica", "Scegli una carta goccia dal campo avversario e posizionala nel tuo, inoltre l'avversario non potrà posizionare carte goccia nel proprio campo per un turno.", Resources.Load<Sprite>("materialipericolosiplaceholder")));
+        cardList.Add(new Carta(12, "Prelievo Sostenibile", "Prendi una carta posizionamento a scelta e inseriscila nella tua mano.", Resources.Load<Sprite>("materialipericolosiplaceholder")));
+        cardList.Add(new Carta(13, "Gestione delle Risorse", "Nel turno successivo l'avversario giocherà a carte scoperte.", Resources.Load<Sprite>("materialipericolosiplaceholder")));
+        cardList.Add(new Carta(14, "Cooperazione", "Pesca una carta casualmente dalla mano dell'avversario, l'avversario fa lo stesso dalla tua mano.", Resources.Load<Sprite>("materialipericolosiplaceholder")));
     }
 }
